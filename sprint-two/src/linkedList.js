812fc28c6c7b3d4b6d3956d27e17c6c04db77001
JUAN PINOL <JuannnCodes@gmail.com>
2022-06-03 16:24:47 -0400

    Implemented a linkedList with a functional instantiation style.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,44 @@
+//I: any value
+//O:
+//C:
+//E:
+
 var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+
+  var newNode = new Node(value);
+    if (this.head === null) {
+      this.head = newNode
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.tail = newNode;
   };
+  // var currentNode = this.head.value;
+
 
   list.removeHead = function() {
+    var deletedHead = this.head.value;
+    this.head = this.head.next;
+    return deletedHead;
   };
 
   list.contains = function(target) {
+
+    var temp = this.head;
+    while ( temp.value === target || temp.next !== null) {
+      if ( temp.value === target) {
+        return true
+      }
+        temp = temp.next;
+    }
+    return false;
   };
 
   return list;
